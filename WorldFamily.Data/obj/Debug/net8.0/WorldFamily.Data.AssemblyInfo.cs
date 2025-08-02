7cd66ca860d4354987274e5fd267b564e81ae179
Mariyan Yordanov <marsrewq@gmail.com>
2025-08-02 08:10:34 +0300

    database fix

--- WorldFamily.Data/obj/Debug/net8.0/WorldFamily.Data.AssemblyInfo.cs
+++ WorldFamily.Data/obj/Debug/net8.0/WorldFamily.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldFamily.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf31958c281d97dccd7955225f71b63dadebcff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c97de93f580e4c3ebd65558afbb7c915a1ad8cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldFamily.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldFamily.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
