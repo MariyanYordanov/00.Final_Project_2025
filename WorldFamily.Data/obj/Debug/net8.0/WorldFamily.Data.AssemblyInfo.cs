af4ffccbd953215fdf71b352011ca1d371aa25e7
Mariyan Yordanov <marsrewq@gmail.com>
2025-08-03 15:16:16 +0300

    add

--- WorldFamily.Data/obj/Debug/net8.0/WorldFamily.Data.AssemblyInfo.cs
+++ WorldFamily.Data/obj/Debug/net8.0/WorldFamily.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldFamily.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eeb6d54967da3cdf86b16c818200ad01198d8c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7226491f0136b963726666830ee17adf633662b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldFamily.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldFamily.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
